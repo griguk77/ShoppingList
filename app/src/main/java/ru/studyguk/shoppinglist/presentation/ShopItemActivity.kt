fc77338c98692180ec610ff911ab8950d930a814
griguk77 <griguk77@gmail.com>
2023-07-20 15:40:15 +0300

    Adding a view model class for the second screen

--- app/src/main/java/ru/studyguk/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/studyguk/shoppinglist/presentation/ShopItemActivity.kt
@@ -8,6 +8,5 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-        //hjv
     }
 }
